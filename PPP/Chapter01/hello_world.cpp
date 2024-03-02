1c857bdb014e607d55d08398a67ccc280f2e60fb
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-03-02 22:51:23 +0000

    Add two projects that take user input and then print it to the screen.

--- PPP/Chapter01/hello_world.cpp
+++
@@ -1,8 +0,0 @@
-// This program outputs the message "Hello, World!" to the monitor
-#include "../std_lib_facilities.h"
-
-int main() // C++ program starts by executing the function main
-{
-	cout << "Hello, World!"; // output "Hello, World!"
-	return 0;
-}
