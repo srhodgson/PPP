e5b9dd134e43b08ef7c989441ce0279bdfad2985
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-06-24 23:27:41 +0100

    Add some files needed for the book + add the obligatory hello world program

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,8 @@
+// This program outputs the message "Hello, World!" to the monitor
+import std;
+using namespace std;
+
+int main() // C++ programs start by executing the function main 
+{
+	std::cout << "Hello, World!\n"; // output "Hello, World!"
+}
