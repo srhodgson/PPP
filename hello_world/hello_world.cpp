5132477797eb47964835e570e73ff5c564ad9e2a
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-06-27 21:46:13 +0100

    remove some files and add include

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -1,8 +1,8 @@
 // This program outputs the message "Hello, World!" to the monitor
-import std;
-using namespace std;
+#include "../std_lib_facilities.h"
 
 int main() // C++ programs start by executing the function main 
 {
 	std::cout << "Hello, World!\n"; // output "Hello, World!"
+	return 0;
 }
