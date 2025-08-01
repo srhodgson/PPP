cb6a864b15746140ab2b7c72a48d4739641a13e6
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-08-01 22:38:00 +0100

    find repeated words improved

--- Part01/Chapter03/repeated_words.cpp
+++ Part01/Chapter03/repeated_words.cpp
@@ -2,12 +2,14 @@
 
 int main ()
 {
+	int number_of_words = 0;
 	string previous = " "; // Previous word; initialised to "not a word"
 	string current;        // Current word
 	while (cin >> current) // Read a stream or words 
 	{
+		++number_of_words;
 		if (previous == current) // Check if the current word is the same as the last 
-			cout << "repeated word: " << current << '\n';
+			cout << "word number " << number_of_words << " repeated: " << current << '\n';
 		previous = current;
 	}
 }
