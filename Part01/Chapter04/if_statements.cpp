d429bb6438a2c8ef46a53f80b14c1125c34ac931
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-08-05 22:41:59 +0100

    Add new chapters and conversion programs
    Updated CMakeLists.txt to include Chapter03 and Chapter04.
    Added executables for if_statements and length_conversion.
    Implemented if_statements.cpp to find the maximum of two integers.
    Created length_conversion.cpp for converting between inches and centimeters.

---
+++ Part01/Chapter04/if_statements.cpp
@@ -0,0 +1,14 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	cout << "Please enter two integers\n";
+	cin >> a >> b;
+
+	if (a < b)
+		cout << "max(" << a << "," << b << ") is " << b << "\n";
+	else
+		cout << "max(" << a << "," << b << ") is " << a << "\n";
+}
