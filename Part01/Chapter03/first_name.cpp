cb3fec9f3176f84bd5207582830fd3bbd5dc19dc
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-12-26 22:47:17 +0000

    Switch to VSCode IDE from CLion
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

---
+++ Part01/Chapter03/first_name.cpp
@@ -0,0 +1,8 @@
+// Read and write a first name
+#include "../../std_lib_facilities.h"
+
+int main() {
+    cout << "Please enter your first name (followed by 'enter':\n";
+    string first_name; // first_name is a variable of type string
+    cin >> first_name; // Read characters into first_name
+}
